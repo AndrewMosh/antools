37c639e4b1ddfde4ef5b8c6f86560c50144a5cb0
Andrew Mosh <malinaenglishclub@gmail.com>
2024-07-04 17:58:12 +0300

    header done

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
         nav.classList.toggle("header__nav-mobile--active");
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdown = document.querySelector(".nav__item--dropdown");
+    const nav = document.querySelector(".nav__dropped-menu");
+
+    dropdown.addEventListener("click", (e) => {
+        console.log(e.target);
+        nav.classList.toggle("nav__active");
+    });
+});
+
+document.addEventListener("click", (e) => {
+    const nav = document.querySelector(".nav__dropped-menu");
+
+    if (e.target.classList.value !== "nav__droppable" && e.target.classList.value !== "nav__link") {
+        nav.classList.remove("nav__active");
+    }
+});
